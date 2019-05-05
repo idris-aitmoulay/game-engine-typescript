e3199864edfa492ac4d445db3cdb706aa5ae4dc0
idris.aitmoulay <idris.aitmoulay@gmail.com>
2019-05-05 17:02:09 +0000

    add GLUtilities (Web Gl Component).

--- src/engine.ts
+++ src/engine.ts
@@ -2,18 +2,17 @@ namespace TSE {
     export  class Engine {
         private _count: number = 0;
         public constructor(){
-
         }
 
         public start(): void {
+            // @ts-ignore
+            GLUtilities.initialize();
             this.loop()
         }
 
         public loop(): void {
             this._count++;
-
             document.title = this._count.toString();
-
             requestAnimationFrame(this.loop.bind(this));
         }
     }
---
+++ src/gl.ts
@@ -0,0 +1,10 @@
+namespace TSE {
+    export var gl: WebGLRenderingContext;
+    export class GLUtilities {
+        public static initialize(elementId?: string): void{
+            if (elementId) {
+                // todo write code source
+            }
+        }
+    }
+}
